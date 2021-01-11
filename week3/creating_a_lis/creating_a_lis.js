f9e4267d4741ed59f52b7479557e57209f11afe6
csherms <casey.sherman@bryanuniversity.edu>
2021-01-10 18:42:26 -0700

    second commit

--- week3/creating_a_lis/creating_a_lis.js
+++ week3/creating_a_lis/creating_a_lis.js
@@ -1,32 +1,39 @@
-// # 2 Create an h1 an append to dom
+// # 2 Create an h1 an append to dom to see if it works.
 
 var myHeader = document.createElement ("h1");
-var headerName = document.getElementById ("head");
+document.body.append (myHeader);
 
 // # 3 -- Create a for loop to repeat header 10 times
 
 for (i = 0; i < 10; i++) {
+    var myHeader = document.createElement ("h1");
     myHeader.innerHTML = "Hello, World!";
-    headerName.innerHTML += "<h1>" + myHeader.innerHTML + 
-    "</h1>";
+    document.body.append(myHeader);
+    
 
 // # 4 Add some styles (still inside the for loop.)
 
-    headerName.style.color = 'blue';
-    headerName.style.fontSize = '20px';
-    headerName.style.fontStyle = 'italic';
-
+    myHeader.style.color = 'blue';
+    myHeader.style.fontSize = '20px';
+    myHeader.style.fontStyle = 'italic';
 }
+
 
 // # 5 Create a new array using the following information.
 
 var namesArray = ['Steve', 'Larry', 'Joe', 'Shirley', 'Steph', 'Nate', 'Rick', 'Emily',];
 
-var newVar = document.getElementById ("ulNames");
-
 // # 6 Create a for loop that iterates through the array.
 
 for (i = 0; i < namesArray.length; i++) {
-    newVar.innerHTML += "<li>" + namesArray[i] + "</li>"
+    var list = document.createElement ("ul");
+namesArray.forEach (function (item) {
+    var li = document.createElement ("li");
+        li.textContent = item;
+        list.appendChild (li);
+});
+        
+    
 }
+document.body.append (list);
 
