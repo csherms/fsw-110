0c58284a9dae938abdb553871128f4eb34eba0fa
csherms <casey.sherman@bryanuniversity.edu>
2021-01-10 19:56:20 -0700

    second commit

--- week3/changing_styles_wJS/index.js
+++ week3/changing_styles_wJS/index.js
@@ -1,26 +1,22 @@
 // 2. Create a list of 5 H2 elements on the DOM using a for loop
 
-var myHeader = document.createElement ("h2");
-var headerRepeat = document.getElementById ("head");
-
-
 for (i = 0; i < 5; i++) {
-    myHeader.innerHTML = "Header";
-    headerRepeat.innerHTML += "<h2>" + myHeader.innerHTML + "</h2>"
+    var myHeader = document.createElement ("h2");
+    myHeader.innerText = "Header";
+    document.body.append (myHeader);
 
 // 3. Using the .style property.
 
-    headerRepeat.style.fontSize = '20px';
-    headerRepeat.style.fontWeight = 'lighter';
-    headerRepeat.style.fontFamily = 'sans-serif';
-    headerRepeat.style.color = 'cornflowerblue';
+    myHeader.style.fontSize = '20px';
+    myHeader.style.fontWeight = 'lighter';
+    myHeader.style.fontFamily = 'sans-serif';
+    myHeader.style.color = 'cornflowerblue';
     
 }
 
 
 // 5. Add class to elements being created using classList
-
-document.querySelector("#head").classList.add("border");
+document.querySelector("h2").classList.add("border");
 
 
 
